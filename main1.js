911ffa09fb52ee7a4ce27092f13653a1dd413527
dimovskiv <60580525+dimovskiv@users.noreply.github.com>
2020-02-17 03:19:34 +0100

    blogfinished

--- main1.js
+++ main1.js
@@ -1,11 +1,14 @@
+AOS.init();
 $(window).on("scroll", function() {
     if($(window).scrollTop() > 200) {
         $(".navbar").addClass("changecolor")
         $('.navbar-brand img').attr("src","./logo/2.png");
+        
       
     } else {
        $(".navbar").removeClass("changecolor");
        $('.navbar-brand img').attr("src","./logo/1.png");
+       
     }
 })
 
