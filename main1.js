45a4088bf84a1cd0efff1247e759517b0935b8bd
dimovskiv <60580525+dimovskiv@users.noreply.github.com>
2020-05-14 05:45:09 +0200

    wafw

--- main1.js
+++ main1.js
@@ -3,12 +3,12 @@ $(window).on("scroll", function() {
     if($(window).scrollTop() > 200) {
         $(".navbar").addClass("changecolor")
         $('.navbar-brand img').attr("src","./logo/2.png");
-        
+        $(".nav-item a").addClass("changecolor1");
       
     } else {
        $(".navbar").removeClass("changecolor");
        $('.navbar-brand img').attr("src","./logo/1.png");
-       
+       $(".nav-item a").removeClass("changecolor1");
     }
 })
 
